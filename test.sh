afb2b2574818554a157e8a80f9a2187753831d31
b.andhana <bekti.andhana@gmail.com>
2021-04-27 16:09:00 +0700

    Update

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ img_folder=$2
 use_cuda=$3
 
 while read line; do
-    `python inference.py --img_path "$img_folder/$line" --use_cuda $use_cuda`
+    `python inference.py --img_path $img_folder/$line --use_cuda $use_cuda`
 done < $filename
