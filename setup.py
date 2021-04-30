88ef7bb2253dd5e03bd7fa9ab39af90d0f8bf6d5
adamsyah <fadillahadam11@gmail.com>
2021-04-30 14:28:04 +0700

    Add the setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Detection and Segmentation Module'
+LONG_DESCRIPTION = 'The object detection module is obtained from Zyolo17 EfficientDet where the segmentation module is obtained from Inside-Outside-Guidance'
+
+# Setting up
+setup(
+    name='pytorch_deseg_module',
+    version=VERSION,
+    author='Fadillah Adamsyah Maani',
+    author_email='fadillahadam11@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    
+    keywords=['python', 'pytorch', 'deep learning', 'object detection',
+              'semantic segmentation', 'Zyolo17 Efficientdet',
+              'Inside-Outside-Guidance'],
+    classifiers=[
+        "Intended Audience :: Any",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
