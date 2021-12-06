7329aea4df55d4278d195bbbf6dac10f023b3f79
fadamsyah <fadillahadam11@gmail.com>
2021-12-06 10:09:22 +0700

    Update default augmentations.py

--- augmentations.py
+++ augmentations.py
@@ -8,9 +8,25 @@ import albumentations as A
 # Efficient-Det input image sizes : [512, 640, 768, 896, 1024, 1280, 1280, 1536, 1536]
 def Albumentations(params):
     augmentation = A.Compose([
-        A.Transpose(p=0.5), A.VerticalFlip(p=0.5), A.HorizontalFlip(p=0.5),
-        A.OneOf([A.CLAHE(p=1.), A.RandomBrightnessContrast(0.15, 0.15, p=1.),A.RandomGamma(p=1.), A.HueSaturationValue(p=1.)], p=0.8),
-        A.OneOf([A.Blur(4, p=1.), A.MotionBlur(5, p=1.), A.Sharpen(p=1.)], p=0.75),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.Transpose(p=0.5),
+        A.OneOf([
+            A.CLAHE(p=1.),
+            A.RandomBrightnessContrast(0.15, 0.15, p=1.),
+            A.RandomGamma(p=1.),
+            A.HueSaturationValue(p=1.)],
+            p=0.5),
+        A.OneOf([
+            A.Blur(3, p=1.),
+            A.MotionBlur(3, p=1.),
+            A.Sharpen(p=1.)],
+            p=0.5),
+        A.ShiftScaleRotate(
+            shift_limit=0.05, scale_limit=0.05, rotate_limit=15,
+            border_mode=cv2.BORDER_CONSTANT, p=0.75)
+        
+        # IMPORTANT: You MUST add A.Normalize(...) in the list.
         A.Normalize(mean=params.mean, std=params.std, max_pixel_value=255., always_apply=True)
     ], bbox_params=A.BboxParams(format='coco', label_fields=['category_ids'], min_visibility=0.2))
     
